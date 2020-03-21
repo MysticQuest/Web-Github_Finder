c40526266757ab3c2b28c3dafa8bcd06652e3ad5
Mystic Quest <l1skou@gmail.com>
2020-03-21 06:43:06 +0200

    search functionality

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -5,11 +5,11 @@ const UserItem = ({ user: { login, avatar_url, html_url } }) => {
 
     return (
         <div className="card text-center">
-            <img src={avatar_url} alt="" style={{ width: '60px' }} className="round-img" />
+            <img src={avatar_url} alt='' style={{ width: '60px' }} className='round-img' />
 
             <h3>{login}</h3>
 
-            <div><a href={html_url} className="btn-dark btn-sm btn my-1"> More </a>
+            <div><a href={html_url} className='btn-dark btn-sm btn my-1'> More </a>
             </div>
         </div>
     )
